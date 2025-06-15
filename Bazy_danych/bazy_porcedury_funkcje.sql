06f3cc201e46531e931010cde21f2b7261c03116
Kacp_r <kacp.r.nalepa@gmail.com>
2025-06-15 22:06:16 +0200

    Update bazy_porcedury_funkcje.sql

--- Bazy_danych/bazy_porcedury_funkcje.sql
+++ Bazy_danych/bazy_porcedury_funkcje.sql
@@ -14,12 +14,19 @@ END$$
 DELIMITER ;
 
 -- 8. Klasyfikacja pracowników na podstawie liczby projektów
-SELECT p.ID_Pracownika, p.Imię, p.Nazwisko,
-       CASE
-           WHEN COUNT(pr.ID_Projektu) >= 5 THEN 'super'
-           WHEN COUNT(pr.ID_Projektu) BETWEEN 2 AND 4 THEN 'jako taki'
-           ELSE 'marny'
-       END AS Ocena
-FROM Pracownicy p
-LEFT JOIN Projekty pr ON p.ID_Pracownika = pr.ID_Pracownika
-GROUP BY p.ID_Pracownika, p.Imię, p.Nazwisko;
+DELIMITER $$
+
+CREATE PROCEDURE OcenaPracownikow()
+BEGIN
+    SELECT p.ID_Pracownika, p.Imię, p.Nazwisko,
+           CASE
+               WHEN COUNT(pr.ID_Projektu) >= 5 THEN 'super'
+               WHEN COUNT(pr.ID_Projektu) BETWEEN 2 AND 4 THEN 'jako taki'
+               ELSE 'marny'
+           END AS Ocena
+    FROM Pracownicy p
+    LEFT JOIN Projekty pr ON p.ID_Pracownika = pr.ID_Pracownika
+    GROUP BY p.ID_Pracownika, p.Imię, p.Nazwisko;
+END$$
+
+DELIMITER ;
